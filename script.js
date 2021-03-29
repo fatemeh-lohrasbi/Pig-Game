e1dd8321bfe476a8569ccfea20ce28d8b4597de5
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-30 02:01:20 +0430

    switch to next player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 /* Selecting elements */
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -14,7 +16,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0,0];
 let currentScore = 0;
+let activePlayer = 0;
 
 /* rolling dice functtionality */
 btnRoll.addEventListener('click', function () {
@@ -32,10 +37,15 @@ btnRoll.addEventListener('click', function () {
     if(diceNum !== 1){
         //if it is not 1 add dice to current score
         currentScore += diceNum;
-        current0El.textContent = currentScore;//change later    
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
 
         // if it is 1 switch to next player
     }else{
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active'); 
 
     }
 });
