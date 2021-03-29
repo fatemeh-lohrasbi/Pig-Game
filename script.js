669813d9ac48f5d923eb3d89777676ccf512cb8a
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-30 02:34:36 +0430

    holding current score

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function(){
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active'); 
+}
+
 /* rolling dice functtionality */
 btnRoll.addEventListener('click', function () {
     // 1. Generating a random dice roll
@@ -41,11 +49,21 @@ btnRoll.addEventListener('click', function () {
 
         // if it is 1 switch to next player
     }else{
-        document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active'); 
+        switchPlayer();
 
     }
 });
+
+btnHold.addEventListener('click', function(){
+    console.log('hold')
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer] ;
+
+    // 2. Check if player's score is > 100
+    // Finish the game
+
+    // switch to the next player
+    switchPlayer();
+
+})
