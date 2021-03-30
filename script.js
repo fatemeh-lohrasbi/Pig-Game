801f519c5c0a9b8257ad608d62f2355c07880aab
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-30 12:43:54 +0430

    reset game button

--- script.js
+++ script.js
@@ -11,13 +11,24 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 /* Starting condition */
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    diceEl.classList.add('hidden');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+init();
 const switchPlayer = function () {
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
     currentScore = 0;
@@ -25,7 +36,7 @@ const switchPlayer = function () {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 }
-/* rolling dice functtionality */
+/* Rolling dice functtionality */
 btnRoll.addEventListener('click', function () {
     if (playing) {
         // 1. Generating a random dice roll
@@ -45,6 +56,7 @@ btnRoll.addEventListener('click', function () {
         }
     }
 });
+/* Hold button */
 btnHold.addEventListener('click', function () {
     if (playing) {
         // 1. Add current score to active player's score
@@ -52,7 +64,7 @@ btnHold.addEventListener('click', function () {
         document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
         // 2. Check if player's score is > 100
         // Finish the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 50) {
             playing = false;
             diceEl.classList.add('hidden');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -63,3 +75,5 @@ btnHold.addEventListener('click', function () {
         }
     }
 })
+/* Reset game */
+btnNew.addEventListener('click', init);
